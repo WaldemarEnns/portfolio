ae17dfeeff6525c59199641390c97297e9ce8162
Waldemar Enns <ennsdesigns96@gmail.com>
2024-08-31 09:34:17 +0200

    Implements a simple navbar

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-
-    <article class="prose lg:prose-xl">
-      Some article text.
-    </article>
-  </div>
+  <Navbar/>
 </template>
---
+++ components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navbar bg-base-100">
+    <div class="navbar-start">
+      <a class="btn btn-ghost text-xl">waldemar enns</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>About me</a></li>
+        <li><a>Service</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <a class="btn">Get in touch</a>
+    </div>
+  </div>
+</template>
