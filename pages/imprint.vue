9a880e2029b07cb6ec789cdac2da99268836138d
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-03 08:39:30 +0200

    Implements imprint

---
+++ pages/imprint.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Impressum',
+  name: 'imprint'
+})
+</script>
+
+<template>
+  <article class="container prose mt-12 text-center m-auto">
+    <h1>Impressum</h1>
+
+    <p>
+      Waldemar Enns<br>
+      Software Development<br>
+      Burggrenstraße 13<br>
+      52068 Aachen
+    </p>
+
+    <h2>Kontakt</h2>
+
+    <p>
+      Telefon: 01736824410<br>
+      E-Mail: kontakt@waldemarenns.de
+    </p>
+
+    <h2>Gewerbeanmeldung</h2>
+
+    <p>
+      Die Gewerbeerlaubnis nach §14 oder §55c GewO wurde am 01.10.2018 von folgender Stelle erteilt:
+      Stadt Aachen
+    </p>
+
+    <h2>Berufsbezeichnung und berufsrechtliche Regelungen</h2>
+
+    <p>
+      Berufsbezeichnung:<br>
+      Softwareentwickler
+      <br>
+      Verliehen in:<br>
+      Deutschland
+    </p>
+
+    <h2>EU-Streitschlichtung</h2>
+    
+    <p>
+      Die Europäische Kommission stellt eine Plattform zur Online-Streitschlichtung (OS) bereit:
+      https://ec.europa.eu/consumers/odr/
+      <br>
+      Unsere E-Mail Adresse finden Sie oben im Impressum.
+    </p>
+
+    <h2>Verbraucher-Streit-Beilegung/Universal-Schlichtungs-Stelle</h2>
+
+    <p>
+      Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle zeilzunehmen.
+    </p>
+  </article>
+</template>
