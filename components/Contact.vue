6a83d46cb9229090884f20a3e9a838a94490446f
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-06 08:59:19 +0200

    Adds name input to contact form

--- components/Contact.vue
+++ components/Contact.vue
@@ -6,13 +6,18 @@
     </article>
     <div class="w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2 2xl:w-1/3">
       <label class="input input-bordered flex items-center gap-2 mb-4">
+        <font-awesome-icon icon="fa-solid fa-user"></font-awesome-icon>
+        <input name="senderName" type="text" class="grow" :placeholder="$t('form.placeholders.name')" />
+      </label>
+      <label class="input input-bordered flex items-center gap-2 mb-4">
         <font-awesome-icon icon="fa-solid fa-envelope"></font-awesome-icon>
-        <input type="email" class="grow" :placeholder="$t('form.placeholders.email')" />
+        <input name="fromMail" type="email" class="grow" :placeholder="$t('form.placeholders.email')" />
       </label>
       <textarea
         class="textarea textarea-bordered w-full mb-4 resize-none"
         :placeholder="$t('form.placeholders.contact_message')"
         rows="4"
+        name="message"
       ></textarea>
       <div class="flex justify-center">
         <button class="btn btn-primary w-full" disabled>
