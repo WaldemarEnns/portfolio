a91eb475b86dfec4d3caea7b33737a62e110e24e
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-13 17:20:24 +0200

    Redesigns the methods section

--- components/Method.vue
+++ components/Method.vue
@@ -9,10 +9,10 @@ defineProps<Props>()
 </script>
 
 <template>
-  <div class="w-full rounded-lg bg-secondary text-primary-content px-12 py-8 flex flex-col items-center sm:items-center md:items-center lg:items-start xl:items-start 2xl:items-start">
+  <div class="w-full border border-secondary rounded-lg bg-primary-content px-12 py-8 flex flex-col items-center sm:items-center md:items-center lg:items-start xl:items-start 2xl:items-start">
     <div class="flex flex-row justify-center items-start">
       <font-awesome-icon :icon="icon" size="2xl" class="mb-8 mr-4"></font-awesome-icon>
-      <h3 class="text-xl font-medium prose text-primary-content" v-if="title">{{ title }}</h3>
+      <h3 class="text-xl font-medium prose" v-if="title">{{ title }}</h3>
     </div>
     <p class="inline-block">{{ content }}</p>
   </div>
