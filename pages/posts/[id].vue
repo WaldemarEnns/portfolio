1ec2cdaa9920d3f47e52c84079b76740584d1402
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-17 12:47:02 +0200

    Adds simple posts page

---
+++ pages/posts/[id].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'post'
+})
+
+const { currentRoute } = useRouter()
+
+const postId = computed(() => currentRoute.value.params.id)
+
+const content = ref('Some Content')
+</script>
+
+<template>
+  <article class="container prose py-12 mx-auto">
+    {{ content }}: {{ postId }}
+  </article>
+</template>
