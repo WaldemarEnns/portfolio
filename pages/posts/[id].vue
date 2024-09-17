c6342ba418afe38aede82f7b724fedb791e9a97c
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-17 13:17:03 +0200

    Implements a simple SSR/PRE post site that queries strapi

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
+import type { Post } from '@/types/strapi'
+const { findOne } = useStrapi<Post>()
+
 definePageMeta({
   layout: 'post'
 })
 
 const { currentRoute } = useRouter()
 
-const postId = computed(() => currentRoute.value.params.id)
+const postId = computed(() => currentRoute.value.params.id as string)
 
-const content = ref('Some Content')
+const { data } = await useAsyncData(
+  'post',
+  () => findOne('posts', postId.value)
+)
+
+const content = computed(() => data.value?.data.attributes.Content)
 </script>
 
 <template>
   <article class="container prose py-12 mx-auto">
-    {{ content }}: {{ postId }}
+    {{ content }}
   </article>
 </template>
