9f33ffd2d29cfc87000d6edbf3573133ee54b713
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-17 12:46:57 +0200

    Adds simple posts layout

---
+++ layouts/post.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const route = useRoute()
+const { t } = useI18n()
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: 'id',
+  addSeoAttributes: true
+})
+
+const title = ref('Post Title')
+const description = ref('Post Description')
+
+</script>
+
+<template>
+  <div>
+    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+      <Head>
+        <Title>{{ title }}</Title>
+        <template v-for="link in head.link" :key="link.id">
+          <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <Link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <template v-for="meta in head.meta" :key="meta.id">
+          <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
+        </template>
+        <Meta v-if="description" name="description" :content="description" />
+      </Head>
+      <Body>
+        <Navbar />
+        <slot />
+        <Footer />
+      </Body>
+    </Html>
+  </div>
+</template>
