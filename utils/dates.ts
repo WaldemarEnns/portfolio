061db3ea56f447bb9af4a0f54b0ce11afd6635c9
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-18 09:28:50 +0200

    Adds docstrings to dates utility function formatToLocaleDate

--- utils/dates.ts
+++ utils/dates.ts
@@ -1,3 +1,9 @@
+
+/**
+ * Format a date to a locale date
+ * @param date - The date to format
+ * @returns The date formatted to a locale date
+ * */
 export function formatToLocaleDate(date: string): string {
   return new Date(date).toLocaleDateString();
 }
