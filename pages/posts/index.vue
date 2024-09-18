f755a4c7dba0f85cc8aaa504e873415a56364f42
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-18 09:27:41 +0200

    Creates blog landing page
    Lists all available blog posts in current locale.

---
+++ pages/posts/index.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import type { Strapi4ResponseData, StrapiLocale } from '@nuxtjs/strapi';
+import type { Post } from '~/types/strapi';
+
+definePageMeta({
+  layout: 'post'
+})
+
+const { locale } = useI18n()
+const { find } = useStrapi<Post>()
+
+const { data } = await useAsyncData(
+  'posts',
+  () => find('posts', {
+    locale: locale.value as StrapiLocale,
+    pagination: {
+      page: 1,
+      pageSize: 20
+    },
+    publicationState: 'preview'
+  }),
+)
+
+const isPublished = (post: Strapi4ResponseData<Post>) => post.attributes.publishedAt !== null
+</script>
+
+<template>
+  <section class="container mx-auto my-12">
+    <h1 class="font-bold text-2xl mb-8">My blogposts in {{ locale }}</h1>
+    <div class="card bg-slate-800"
+      v-for="post in data?.data"
+      :key="post.id"
+    >
+      <div class="card-body flex flex-row justify-between items-center">
+        <div>
+          <h2 class="card-title block">{{ post.attributes.Title }}</h2>
+          <i class="block mt-2">
+            {{ isPublished(post) ? 'Published' : 'Updated' }} on
+            {{ formatToLocaleDate(post.attributes.publishedAt || post.attributes.updatedAt) }}
+          </i>
+        </div>
+        <NuxtLink :to="`/posts/${post.id}`" class="btn btn-primary btn-outline btn-wide">Have a read</NuxtLink>
+      </div>
+    </div>
+
+    <div class="flex flex-row justify-center items-start">
+      <div class="join mt-4">
+        <button
+          v-for="i in data?.meta.pagination.total"
+          class="join-item btn"
+        >
+          {{ i }}
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
