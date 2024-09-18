6367d7c2797238cc4a940dea297fdc7a699ebc70
Waldemar Enns <ennsdesigns96@gmail.com>
2024-09-18 08:09:53 +0200

    Updates post page

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import type { Post } from '@/types/strapi'
+import { StrapiBlocks } from 'vue-strapi-blocks-renderer'
+
 const { findOne } = useStrapi<Post>()
 
 definePageMeta({
@@ -15,11 +17,28 @@ const { data } = await useAsyncData(
   () => findOne('posts', postId.value)
 )
 
-const content = computed(() => data.value?.data.attributes.Content)
+const content = computed(() => data.value?.data?.attributes?.Content)
+const title = computed(() => data.value?.data?.attributes?.Title)
+
+const publicationDate = computed(() => {
+  if (data.value?.data?.attributes?.publishedAt) {
+    return new Date(data.value?.data.attributes.publishedAt).toLocaleDateString()
+  } else {
+    return null
+  }
+})
+
+const updatedAt = computed(() => new Date(data.value?.data?.attributes?.updatedAt!).toLocaleDateString())
 </script>
 
 <template>
   <article class="container prose py-12 mx-auto">
-    {{ content }}
+    <i v-if="publicationDate">Veröffentlicht am {{ publicationDate }}</i>
+    <i v-else>Zuletzt bearbeitet am {{ updatedAt }}</i>
+    <h1>{{ title }}</h1>
+    <StrapiBlocks
+      v-if="content"
+      :content="content"
+    />
   </article>
 </template>
