23a0da15fa3e2fb7236087c03d1f5af5f37f988d
Waldemar Enns <ennsdesigns96@gmail.com>
2024-10-16 10:45:54 +0200

    Ignore plausible on localhost

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,10 @@ export default defineNuxtConfig({
     }
   },
 
+  plausible: {
+    ignoredHostnames: ['localhost'],
+  },
+
   routeRules: {
     '/': { prerender: true },
     '/imprint': { prerender: true },
