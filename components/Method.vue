2563017ae6a084436c77ac06f616bb2601b6d129
Waldemar Enns <ennsdesigns96@gmail.com>
2024-12-25 14:35:49 +0100

    Adjusts Method cards to align to the general styles

--- components/Method.vue
+++ components/Method.vue
@@ -10,9 +10,9 @@ defineProps<Props>()
 
 <template>
   <div class="w-full border border-secondary rounded-lg bg-base-300 px-12 py-8 flex flex-col items-center sm:items-center md:items-center lg:items-start xl:items-start 2xl:items-start">
-    <div class="flex flex-row justify-center items-start">
-      <font-awesome-icon :icon="icon" size="2xl" class="mb-8 mr-4"></font-awesome-icon>
-      <h3 class="text-xl font-medium prose" v-if="title">{{ title }}</h3>
+    <div class="flex flex-row justify-center items-center mb-4">
+      <font-awesome-icon :icon="icon" class="mr-4"></font-awesome-icon>
+      <h2 class="card-title" v-if="title">{{ title }}</h2>
     </div>
     <p class="inline-block">{{ content }}</p>
   </div>
