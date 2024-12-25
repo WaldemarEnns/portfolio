fd6e3c6cb3719f2711cb5e4688fbaf8f437fc6f6
Waldemar Enns <ennsdesigns96@gmail.com>
2024-12-25 14:30:59 +0100

    Aligns Method-Cards bg-color to services bg-color

--- components/Method.vue
+++ components/Method.vue
@@ -9,7 +9,7 @@ defineProps<Props>()
 </script>
 
 <template>
-  <div class="w-full border border-secondary rounded-lg bg-primary-content px-12 py-8 flex flex-col items-center sm:items-center md:items-center lg:items-start xl:items-start 2xl:items-start">
+  <div class="w-full border border-secondary rounded-lg bg-base-300 px-12 py-8 flex flex-col items-center sm:items-center md:items-center lg:items-start xl:items-start 2xl:items-start">
     <div class="flex flex-row justify-center items-start">
       <font-awesome-icon :icon="icon" size="2xl" class="mb-8 mr-4"></font-awesome-icon>
       <h3 class="text-xl font-medium prose" v-if="title">{{ title }}</h3>
