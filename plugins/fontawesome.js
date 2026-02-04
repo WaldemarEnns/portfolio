6f6dbaa7e8c8f33d95a966b7c215e0e6ebc0ad8a
WaldemarEnns <ennsdesigns96@gmail.com>
2026-02-04 08:13:32 +0100

    perf(icons): tree-shake FontAwesome imports
    Replace bulk icon library imports with specific icon imports to
    significantly reduce bundle size. Only imports the ~35 icons actually
    used in the application instead of the entire fas/fab/far libraries.
    
    Reduces FontAwesome bundle contribution from ~100KB+ to ~10KB.
    
    Closes #63

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,15 +1,106 @@
-// For Nuxt 3
+// For Nuxt 3 - Tree-shaken FontAwesome imports
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+
+// Solid icons (fa-solid)
+import {
+  faArrowLeft,
+  faArrowRight,
+  faArrowUp,
+  faBars,
+  faBolt,
+  faBookOpen,
+  faBullseye,
+  faBuilding,
+  faCalendar as faCalendarSolid,
+  faChalkboardUser,
+  faCheck,
+  faChartLine,
+  faCode,
+  faEnvelope,
+  faEnvelopeOpen,
+  faExclamationTriangle,
+  faGraduationCap,
+  faHandshake,
+  faHome,
+  faICursor,
+  faMessage,
+  faPaperPlane,
+  faRightFromBracket,
+  faRobot,
+  faRocket,
+  faStar,
+  faTerminal,
+  faUser,
+  faUsers,
+  faXmark
+} from '@fortawesome/free-solid-svg-icons'
+
+// Regular icons (fa-regular)
+import {
+  faCalendar as faCalendarRegular,
+  faClock,
+  faNewspaper
+} from '@fortawesome/free-regular-svg-icons'
+
+// Brand icons (fa-brands)
+import {
+  faGithub,
+  faLaravel,
+  faLinkedin,
+  faVuejs
+} from '@fortawesome/free-brands-svg-icons'
 
 config.autoAddCss = false
 
-library.add(fas)
-library.add(fab)
-library.add(far)
+// Add solid icons
+library.add(
+  faArrowLeft,
+  faArrowRight,
+  faArrowUp,
+  faBars,
+  faBolt,
+  faBookOpen,
+  faBullseye,
+  faBuilding,
+  faCalendarSolid,
+  faChalkboardUser,
+  faCheck,
+  faChartLine,
+  faCode,
+  faEnvelope,
+  faEnvelopeOpen,
+  faExclamationTriangle,
+  faGraduationCap,
+  faHandshake,
+  faHome,
+  faICursor,
+  faMessage,
+  faPaperPlane,
+  faRightFromBracket,
+  faRobot,
+  faRocket,
+  faStar,
+  faTerminal,
+  faUser,
+  faUsers,
+  faXmark
+)
+
+// Add regular icons
+library.add(
+  faCalendarRegular,
+  faClock,
+  faNewspaper
+)
+
+// Add brand icons
+library.add(
+  faGithub,
+  faLaravel,
+  faLinkedin,
+  faVuejs
+)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
